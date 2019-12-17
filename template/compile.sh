9250650dc099b4250b61e0e240c627d16b350aab
wilesduan <duanlj85@126.com>
2019-12-17 10:41:18 +0800

    fix error spell

--- template/compile.sh
+++ template/compile.sh
@@ -12,7 +12,7 @@ dir=`basename $1 .proto`
 
 PROJECT_HOME=
 echo $filename $dir
-$(PROJECT_HOME)/quick-start-app/build/bin/codegen -f $filename -o $dir
+$PROJECT_HOME/quick-start-app/build/bin/codegen -f $filename -o $dir
 succ=`echo $?`
 if [ $succ -ne 0 ]
 then
@@ -23,7 +23,7 @@ fi
 cp $dir/gen_proto/gen_$filename .
 #cp ../../dep_libs/libsrvkit/include/blink.proto .
 echo $dir/gen_proto/gen_$filename
-$(PROJECT_HOME)/quick-start-app/proto/service/protoc --cpp_out=. gen_$filename
+$PROJECT_HOME/quick-start-app/proto/service/protoc --cpp_out=. gen_$filename
 succ=`echo $?`
 if [ $succ -ne 0 ]
 then
